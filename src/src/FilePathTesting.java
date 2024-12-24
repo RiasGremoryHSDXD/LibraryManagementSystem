15a3d89c5206e7af902700656cc758ccdebb16cb
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2024-12-24 14:26:28 +0800

    Delete Unnecessary File

--- src/src/FilePathTesting.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.*;
-public class FilePathTesting {
-    public static void main(String[] args)
-    {
-        File file = new File("src/ImageDirectory/person_logo.png");
-        System.out.println("File Exist: " + file.exists());
-    }
-}
